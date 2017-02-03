7363aade8f64e6d9a84e4c659f7c43d1d35494c2
lpuettmann <lukas.puettmann@uni-bonn.de>
2017-02-03 08:28:04 +0100

    make plot for different length of calculated series

--- ramanujan.m
+++ ramanujan.m
@@ -2,13 +2,25 @@ clear all
 close all
 clc
 
-N = 10; % number of series elements
+N = 30; % number of series elements
 set(0, 'RecursionLimit', N*N)
 x = 2; 
 
-tic
-val = iter_sqrt(x, N);
+val = zeros(N, 1);
 
-disp('____________')
-fprintf('Value: %3.5f.\n', val)
-fprintf('Time: %3.5f seconds.\n', toc)
+for i = 1:N
+
+    tic
+    val(i) = iter_sqrt(x, i);
+
+    fprintf('[%d] Value: %3.5f. (%3.2fs)\n', i, val(i), toc)
+end
+
+figure
+hax = axes;
+plot(val, 'Color', [0.3 0.3 0.3], 'Linewidth', 1.9)
+ylim([0, 3.4])
+xlabel('Number of $$N$$', 'Interpreter', 'Latex', 'Fontsize', 16)
+ylabel('Sum of series', 'Interpreter', 'Latex', 'Fontsize', 16)
+hold on;
+line(get(hax,'XLim'), [3 3], 'Color',[0.5 0.5 0.5], 'Linestyle', '--')
