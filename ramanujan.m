5c9f08b33de6e728639a7ee99b5238ff16a52316
lpuettmann <lukas.puettmann@uni-bonn.de>
2017-02-05 22:15:08 +0100

    rename misleading var name

--- ramanujan.m
+++ ramanujan.m
@@ -6,19 +6,19 @@ N = 30; % number of series elements
 set(0, 'RecursionLimit', N*N)
 x = 2; 
 
-val = zeros(N, 1);
+keep_val = zeros(N, 1);
 
 for i = 1:N
 
     tic
-    val(i) = iter_sqrt(x, i);
+    keep_val(i) = iter_sqrt(x, i);
 
-    fprintf('[%d] Value: %3.5f. (%3.2fs)\n', i, val(i), toc)
+    fprintf('[%d] Value: %3.5f. (%3.2fs)\n', i, keep_val(i), toc)
 end
 
 figure
 hax = axes;
-plot(val, 'Color', [0.3 0.3 0.3], 'Linewidth', 2.1)
+plot(keep_val, 'Color', [0.3 0.3 0.3], 'Linewidth', 2.1)
 ylim([0, 3.4])
 xlabel('Number of N', 'Fontsize', 16, 'Fontweight', 'bold')
 ylabel('Sum of series', 'Fontsize', 16, 'Fontweight', 'bold')
